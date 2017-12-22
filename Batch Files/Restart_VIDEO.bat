931a7508d9d540ffbfeba6cef72a9c56e6446419
mmccarn <mmccarn-github@mmsionline.us>
2017-12-22 11:27:39 -0500

    stop and restart windows explorer; fix indentation
    The mouse pointer was mmisbehaving -- but only in the Start menu -- after resetting the video.  Restarting explorer solves that problem.

--- Batch Files/Restart_VIDEO.bat
+++ Batch Files/Restart_VIDEO.bat
@@ -7,23 +7,30 @@ setlocal enabledelayedexpansion
 
 : iterate through all video controllers identified by wmic
 for /f "tokens=2 delims=, usebackq eol=" %%v in (`wmic path win32_VideoController get name /format:csv ^|findstr /I "%computername%"`) do (
-	set VideoName=%%v
-	: The video name set above includes an ascii 13 (carriage return)
-	: "...:~0,-1..." below removes the extra char
-	wmic path Win32_PNPEntity where "Name='!VideoName:~0,-1!'" call disable
-	wmic path Win32_PNPEntity where "Name='!VideoName:~0,-1!'" call enable
-	)
+  set VideoName=%%v
+  : The video name set above includes an ascii 13 (carriage return)
+  : "...:~0,-1..." below removes the extra char
+  wmic path Win32_PNPEntity where "Name='!VideoName:~0,-1!'" call disable
+  wmic path Win32_PNPEntity where "Name='!VideoName:~0,-1!'" call enable
+  )
+#
+# Kill and restart windows explorer 
+# (otherwise, the mouse isn't pointing where it thinks it is
+#  -- but only in the start menu...)
+taskkill /f /im explorer*
+"%windir%\Explorer.exe"
+
 goto :eof
 
 :notadmin
-	: create a vbs script to get elevated privilege 
-	: then re-run this script (%~s0)
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1  >> "%temp%\getadmin.vbs"
-	echo Set objShell = CreateObject^("WScript.Shell"^)        > "%temp%\activate.vbs"
-	echo objShell.AppActivate "Video Reset" >> "%temp%\activate.vbs"
-	"%temp%\getadmin.vbs"
-	"%temp%\activate.vbs"
-    del "%temp%\getadmin.vbs"
-	del "%temp%\activate.vbs"
-    exit /B
+  : create a vbs script to get elevated privilege 
+  : then re-run this script (%~s0)
+  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+  echo UAC.ShellExecute "%~s0", "", "", "runas", 1  >> "%temp%\getadmin.vbs"
+  echo Set objShell = CreateObject^("WScript.Shell"^)        > "%temp%\activate.vbs"
+  echo objShell.AppActivate "Video Reset" >> "%temp%\activate.vbs"
+  "%temp%\getadmin.vbs"
+  "%temp%\activate.vbs"
+  del "%temp%\getadmin.vbs"
+  del "%temp%\activate.vbs"
+  exit /B
